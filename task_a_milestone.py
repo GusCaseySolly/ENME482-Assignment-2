0ad5b840dbac5f0e660f15fae2aec22bf7ab7268
fsmcnally <fsmcnally@gmail.com>
2025-10-12 13:57:23 +1300

    Update task_a_milestone.py

--- task_a_milestone.py
+++ task_a_milestone.py
@@ -22,16 +22,17 @@ tls = tools.Tools(RDK)
 
 #DEFINE MATRICES HERE
 # define a joint angle array for an intermediate point: theta_1, theta_2, ..., theta_6 (from base to tool)
-J_intermediatepoint_1 = [-18.814643, -106.263000, -127.579643, -125.504571, -33.759643, 139.000000]
-J_intermediatepoint_2 = [-7.332113, -106.007468, -126.225228, -123.837868, -36.251128, 136.406864]
+J_intermediatepoint_1 = [-8.840000, -84.630000, -145.260000, -135.160000, -44.210000, -221.05]
+#J_intermediatepoint_1 = [-18.814643, -106.263000, -127.579643, -125.504571, -33.759643, 139.000000]
+J_intermediatepoint_2 = [-7.332113, -106.007468, -126.225228, -123.837868, -36.251128, -221.05]
 #J_intermediatepoint_3 = [143.035341, -76.864448, 132.479957, -58.497983, 112.188127, 139.269728]
 
 
 
 #Define the position where the rancilio tool sits on the mazzer scale 
 
-offsetleft= 4
-offsetforward = 0
+offsetleft= 3.5
+offsetforward = -2.7
 offsetvert = -4
    
 UR_T_MZS = np.array([[ 0.5,     0.86666,    0,   440.7 ],
@@ -44,6 +45,11 @@ MZS_T_BBA = np.array([[             0,            0,    1,   -10.6 +offsetforwar
                  [              1,               0,     0,   14.2 +offsetvert ],
                  [              0,               0,    0,         1.000000 ]])
 
+
+MZS_T_BBA_eject = np.array([[             0,            0,    1,   -10.6 +offsetforward - 60 ],
+                 [               0,              -1 ,    0,  -21.4 +offsetleft],
+                 [              1,               0,     0,   14.2 +offsetvert ],
+                 [              0,               0,    0,         1.000000 ]])
 
 
 RCT_T_BBI = np.array([[             1,            0,    0,   -32 ],
@@ -60,11 +66,14 @@ TCP_T_RCT = np.array([[ np.cos(-50 * np.pi/180),     -np.sin(-50 * np.pi/180),  
 
 UR_T_TCP = UR_T_MZS @ MZS_T_BBA @ np.linalg.inv(RCT_T_BBI) @ np.linalg.inv(TCP_T_RCT )
 
-
+UR_T_TCP_e = UR_T_MZS @ MZS_T_BBA_eject @ np.linalg.inv(RCT_T_BBI) @ np.linalg.inv(TCP_T_RCT )
                         
 # convert numpy array into an RDK matrix
 T0 = rm.Mat(UR_T_TCP.tolist())
 T = robodk.UR_2_Pose(robodk.Pose_2_UR(T0))
+
+T0_eject = rm.Mat(UR_T_TCP_e.tolist())
+T_e = robodk.UR_2_Pose(robodk.Pose_2_UR(T0_eject))
 
 
 # reset the sim
@@ -88,9 +97,10 @@ UR5.MoveL(T, blocking=True)
 
 time.sleep(1)
 tls.student_tool_detach()
-
 time.sleep(1)
-UR5.MoveJ(J_intermediatepoint_2, blocking=True)
+UR5.MoveL(T_e, blocking=True)
+
+
 UR5.MoveJ(J_intermediatepoint_1, blocking=True)
 # go back home
 UR5.MoveJ(RDK.Item("Home_R", ITEM_TYPE_TARGET), True)
