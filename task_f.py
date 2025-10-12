d9df787d484db407a12f66f7f2a6820abe90e867
fsmcnally <fsmcnally@gmail.com>
2025-10-12 17:12:18 +1300

    Update task_f.py

--- task_f.py
+++ task_f.py
@@ -29,9 +29,8 @@ J_intermediatepoint_2 = [-7.960000, -99.450000, -133.260000, -125.470000, -35.18
 
 
 #Define the position where the rancilio tool sits on the mazzer scale 
-
-offsetleft= 4
-offsetforward = 0
+offsetleft= 3.5
+offsetforward = -2.7
 offsetvert = -4
    
 UR_T_MZS = np.array([[ 0.5,     0.86666,    0,   440.7 ],
@@ -44,6 +43,11 @@ MZS_T_BBA = np.array([[             0,            0,    1,   -10.6 +offsetforwar
                  [              1,               0,     0,   14.2 +offsetvert ],
                  [              0,               0,    0,         1.000000 ]])
 
+
+MZS_T_BBA_eject = np.array([[             0,            0,    1,   -10.6 +offsetforward - 60 ],
+                 [               0,              -1 ,    0,  -21.4 +offsetleft],
+                 [              1,               0,     0,   14.2 +offsetvert ],
+                 [              0,               0,    0,         1.000000 ]])
 
 
 RCT_T_BBI = np.array([[             1,            0,    0,   -32 ],
@@ -60,35 +64,61 @@ TCP_T_RCT = np.array([[ np.cos(-50 * np.pi/180),     -np.sin(-50 * np.pi/180),  
 
 UR_T_TCP = UR_T_MZS @ MZS_T_BBA @ np.linalg.inv(RCT_T_BBI) @ np.linalg.inv(TCP_T_RCT )
 
-
+UR_T_TCP_e = UR_T_MZS @ MZS_T_BBA_eject @ np.linalg.inv(RCT_T_BBI) @ np.linalg.inv(TCP_T_RCT )
                         
 # convert numpy array into an RDK matrix
 T0 = rm.Mat(UR_T_TCP.tolist())
 T = robodk.UR_2_Pose(robodk.Pose_2_UR(T0))
 
+T0_eject = rm.Mat(UR_T_TCP_e.tolist())
+T_e = robodk.UR_2_Pose(robodk.Pose_2_UR(T0_eject))
+
+
 
 # reset the sim
-robot_program = RDK.Item("Reset_Simulation_R", ITEM_TYPE_PROGRAM)
+robot_program = RDK.Item("Reset_Simulation_L", ITEM_TYPE_PROGRAM)
 robot_program.RunCode()
 robot_program.WaitFinished()
+
+#Stls.rancilio_tool_attach_l_ati()
 
 
 # joint movement using a joint array
 
-#UR5.MoveJ(J_intermediatepoint_1, blocking=True)
-UR5.MoveJ(J_intermediatepoint_2, blocking=True)
+flip = [145.260000, -82.110000, 140.210000, -61.890000, 114.950000, 137.680000]
+UR5.MoveJ(flip, blocking=True)
 
-time.sleep(1)
+#UR5.MoveJ(J_intermediatepoint_1, blocking=True)
+#UR5.MoveJ(J_intermediatepoint_2, blocking=True)
+
+#time.sleep(1)
 #UR5.MoveJ(J_intermediatepoint_3, blocking=True)
+
+
+
+
+UR5.MoveL(T_e, blocking=True)
+
 
 # linear movement using an HT matrix
 UR5.MoveL(T, blocking=True)
 
+
+
+
+
+
 time.sleep(1)
 tls.student_tool_attach()
 
-time.sleep(1)
-UR5.MoveJ(J_intermediatepoint_2, blocking=True)
-UR5.MoveJ(J_intermediatepoint_1, blocking=True)
+raised = [141.180000, -77.050000, 130.140000, -55.770000, 110.860000, 139.410000]
+UR5.MoveL(raised, blocking=True)
+
+
+J_intermediatepoint_1 = [-8.840000, -84.630000, -145.260000, -135.160000, -44.210000, -221.05]
+UR5.MoveL(J_intermediatepoint_1, blocking=True)
+#time.sleep(1)
+#UR5.MoveJ(J_intermediatepoint_2, blocking=True)
+#UR5.MoveJ(J_intermediatepoint_1, blocking=True)
 # go back home
-UR5.MoveJ(RDK.Item("Home_R", ITEM_TYPE_TARGET), True)
+#UR5.MoveJ(RDK.Item("Home_R", ITEM_TYPE_TARGET), True)
